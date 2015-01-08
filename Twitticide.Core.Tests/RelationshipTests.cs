190b0081a968ba78af72399cc13f0c1e17a01ab3
Nathan Chere <nathan.chere@tretton37.com>
2015-01-08 17:02:05 +0100

    Ditch xUnit, sick of setup bullshit. First tests with nUnit

---
+++ Twitticide.Core.Tests/RelationshipTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace Twitticide
+{
+    [TestFixture]
+    public class RelationshipTests
+    {
+        [Test]
+        public void Relationship_reports_NotFollowing_correctly()
+        {
+            var target = new Relationship();
+            target.UpdateFollowStatus(false);
+            Assert.AreEqual(Relationship.StatusEnum.NotFollowing, target.Status);
+        }
+
+        [Test]
+        public void Relationship_reports_NotFollowing_correctly_with_no_events()
+        {
+            var target = new Relationship();
+
+            Assert.AreEqual(Relationship.StatusEnum.NotFollowing, target.Status);
+        }
+
+        [Test]
+        public void Relationship_reports_Following_correctly()
+        {
+            var target = new Relationship();
+            target.UpdateFollowStatus(true);
+            Assert.AreEqual(Relationship.StatusEnum.NotFollowing, target.Status);
+        }
+
+        [Test]
+        public void Relationship_reports_Following_correctly_after_refollow()
+        {
+            var target = new Relationship();
+            target.UpdateFollowStatus(true);
+            target.UpdateFollowStatus(false);
+            target.UpdateFollowStatus(true);
+            Assert.AreEqual(Relationship.StatusEnum.Following, target.Status);
+        }
+
+        [Test]
+        public void Relationship_reports_Unfollowed_correctly()
+        {
+            var target = new Relationship();
+            target.UpdateFollowStatus(true);
+            target.UpdateFollowStatus(false);
+            Assert.AreEqual(Relationship.StatusEnum.NotFollowing, target.Status);
+        }
+    }
+}
