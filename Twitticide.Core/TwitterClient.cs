948e8171618dbdc051f1dc51a3c6cbfaaa71dda4
Nathan Chere <git@nathanchere.com.au>
2015-01-08 12:14:03 +0100

    TwitterClient base

---
+++ Twitticide.Core/TwitterClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitticide
+{
+    public class TwitterClient
+    {
+        private const string REG_PATH = @"HKEY_CURRENT_USER\SOFTWARE\NathanChere\Twitticide";
+
+    }
+}
