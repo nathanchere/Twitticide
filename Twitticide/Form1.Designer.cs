66576e597f822d1b89c2dfad6e2d8f545cd08f45
Nathan Chere <git@nathanchere.com.au>
2015-01-09 11:52:52 +0100

    Halfway

--- Twitticide/Form1.Designer.cs
+++ Twitticide/Form1.Designer.cs
@@ -28,21 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.AccountTabs = new DevExpress.XtraTab.XtraTabControl();
+            this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
+            this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.AccountTabs)).BeginInit();
+            this.AccountTabs.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // AccountTabs
+            // 
+            this.AccountTabs.Location = new System.Drawing.Point(153, 131);
+            this.AccountTabs.Name = "AccountTabs";
+            this.AccountTabs.SelectedTabPage = this.xtraTabPage1;
+            this.AccountTabs.Size = new System.Drawing.Size(727, 434);
+            this.AccountTabs.TabIndex = 0;
+            this.AccountTabs.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.xtraTabPage1,
+            this.xtraTabPage2});
+            // 
+            // xtraTabPage1
+            // 
+            this.xtraTabPage1.Name = "xtraTabPage1";
+            this.xtraTabPage1.Size = new System.Drawing.Size(721, 406);
+            this.xtraTabPage1.Text = "xtraTabPage1";
+            // 
+            // xtraTabPage2
+            // 
+            this.xtraTabPage2.Name = "xtraTabPage2";
+            this.xtraTabPage2.Size = new System.Drawing.Size(294, 272);
+            this.xtraTabPage2.Text = "xtraTabPage2";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(730, 453);
+            this.ClientSize = new System.Drawing.Size(1055, 611);
+            this.Controls.Add(this.AccountTabs);
             this.Name = "frmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.AccountTabs)).EndInit();
+            this.AccountTabs.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevExpress.XtraTab.XtraTabControl AccountTabs;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
 
     }
 }
