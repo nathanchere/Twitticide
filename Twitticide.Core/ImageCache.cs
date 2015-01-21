632e0f92d3105032bc79d5dab19e8f4c8b5b65b7
Nathan Chere <git@nathanchere.com.au>
2015-01-21 15:41:17 +0100

    One cache to rule them all

--- Twitticide.Core/ImageCache.cs
+++ Twitticide.Core/ImageCache.cs
@@ -7,44 +7,91 @@ using System.Net;
 using System.Threading.Tasks;
 
 namespace Twitticide
-{/*
-    public class ImageCache : IImageCache  
-    {       
+{
+    public class ImageCache : IImageCache
+    {
+        private class TimestampedImage
+        {
+            public DateTime WhenUpdated { get; set; }
+            public Bitmap Image { get; set; }
+
+            public TimestampedImage(Bitmap image)
+            {
+                Image = image;
+                WhenUpdated = DateTime.Now;
+            }
+        }
+
         public ImageCache(IDataStore dataStore)
         {
             _dataStore = dataStore;
-            _icons = new ConcurrentDictionary<long, Bitmap>();
-            _placeholderBitmap = new Bitmap(3,3);
+            _queue = new ConcurrentDictionary<long, TwitterProfile>();
+            _icons = new ConcurrentDictionary<long, TimestampedImage>();
+            _placeholderBitmap = new Bitmap(3, 3);
             var graphics = Graphics.FromImage(_placeholderBitmap);
             graphics.Clear(Color.Red);
 
+            ReloadCache();
         }
 
-        private IDataStore _dataStore;
-        private Bitmap _placeholderBitmap;
-        private readonly ConcurrentDictionary<long, Bitmap> _icons;
-        private Guid _guid = Guid.NewGuid();
-                
+        private void ReloadCache()
+        {
+            foreach (var result in _dataStore.GetAllAvatars())
+            {
+                _icons.TryAdd(result.Item1, new TimestampedImage(result.Item2));
+            }
+        }
+
+        private readonly ConcurrentDictionary<long, TwitterProfile> _queue;
+        private readonly ConcurrentDictionary<long, TimestampedImage> _icons;
+        private readonly Bitmap _placeholderBitmap;
+        private readonly IDataStore _dataStore;
+
+        private Task _updateThread = null;
+
+        private void RefreshCache()
+        {
+            while (_queue.Any())
+            {
+                var id = _queue.First().Key;
+                TwitterProfile profile;
+                _queue.TryRemove(id, out profile);
+
+                Debug.WriteLine("ImageCache fetching profile image " + id);
+
+                try
+                {
+                    var request = WebRequest.Create(profile.ProfileImageUrl);
+                    using (var response = request.GetResponse())
+                    using (var stream = response.GetResponseStream())
+                    {   
+                        var image= new Bitmap(Image.FromStream(stream), 64, 64);
+                        _icons[profile.Id] = new TimestampedImage(image);
+                        _dataStore.SaveAvatar(profile.Id, image);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debugger.Break();
+                    //_icons[profile.Id, Properties.Resources.Avatar_Missing);
+                }
+            }
+        }
+
         /// <summary>
         /// Tells the cache to fetch and cache the image for a given profile
         /// </summary>
         public void UpdateCache(TwitterProfile profile)
         {
-            if (_icons.ContainsKey(profile.Id)) return;
+            if (!_queue.ContainsKey(profile.Id) && !_icons.ContainsKey(profile.Id)) _queue[profile.Id] = profile;
 
-            try
+            // Start the worker thread if not already working
+            if (_updateThread == null || _updateThread.IsCanceled || _updateThread.IsCompleted ||
+                _updateThread.IsFaulted)
             {
-                var request = WebRequest.Create(profile.ProfileImageUrl);
-                using (var response = request.GetResponse())
-                using (var stream = response.GetResponseStream())
-                {
-                    _icons[profile.Id] = new Bitmap(Image.FromStream(stream), 64, 64);
-                }
+                _updateThread = new Task(RefreshCache);
+                _updateThread.Start();
             }
-            catch (Exception ex)
-            {
-                Debugger.Break();
-            }        
         }
 
         public Bitmap GetAvatar(long id)
@@ -55,9 +102,9 @@ namespace Twitticide
                 {
                     return GetDefaultAvatar();
                 }
-                return _icons[id];
+                return _icons[id].Image;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return GetDefaultAvatar();
             }
@@ -69,5 +116,4 @@ namespace Twitticide
             //return Properties.Resources.Avatar_Missing;
         }
     }
-  * */
 }
